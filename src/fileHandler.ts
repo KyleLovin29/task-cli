5c52a1da104d2227560a565d3dd66733687263e5
KyleLovin29 <ky1e1ovin4@gmail.com>
2025-03-31 21:30:30 -0600

    Started adding tasks logic

---
+++ src/fileHandler.ts
@@ -0,0 +1,9 @@
+import fs from 'fs';
+
+export function Check(path: string) {
+	
+}
+
+export function Write(path: string) {
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,38 @@
 #! node
-const hello = () => {
+import { Command } from 'commander';
+import fs from 'fs';
 
+const program = new Command();
+
+type Task = {
+	id: number,
+	description: string,
+	status: 'todo' | 'in-progress' | 'done',
+	createdAt: Date,
+	modifiedAt: Date,
 }
 
-console.log("Hello");
+program
+	.name("Task CLI")
+	.description("Add Tasks")
+	.version("0.0.1")
+	.command('add <task>')
+	.description('Add a task')
+	.action((task) => {
+		let currentDate = Date.now;
+		let userTask: Task = {
+			id: 2,
+			description: task,
+			status: 'todo',
+			createdAt: new Date(),
+			modifiedAt: new Date()
+		}
+	})
+
+program
+	.command('update <number>')
+	.description('Update a task')
+
+
+
+program.parse(process.argv);
