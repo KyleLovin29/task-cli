cea6c55ed62021515705e47186b8bf8d83e20f42
KyleLovin29 <ky1e1ovin4@gmail.com>
2025-04-04 11:32:15 -0600

    Add tasks.json file to gitignore. Tidied Commander code.

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,19 @@ const filePath = "./tasks.json";
 
 const program = new Command();
 
+program.name("Task CLI").description("Add Tasks").version("0.0.1");
+
 program
-  .name("Task CLI")
-  .description("Add Tasks")
-  .version("0.0.1")
   .command("add <task>")
   .description("Add a task")
   .action((task: string) => {
     fileHandler.WriteToFile(taskHandler.ConvertInputToTask(task), filePath);
   });
-
-program.command("update <number>").description("Update task by ID").option('-p --in-progress', 'Mark selected task as in-progress').option('-d --done', 'Mark selected task as done');
+program
+  .command("update <number>")
+  .description("Update task by ID")
+  .option("-p --in-progress", "Mark selected task as in-progress")
+  .option("-d --done", "Mark selected task as done");
 program.command("delete <number>").description("Delete task by ID");
 program.command("lt").description("List all tasks");
 program.command("ltd").description("List all tasks marked as done");
